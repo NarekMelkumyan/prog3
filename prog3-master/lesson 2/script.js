55a5a364bd67f0daf8937f93d2736afc82cee7f0
NarekMelkumyan <50113513+NarekMelkumyan@users.noreply.github.com>
2019-05-06 17:05:31 +0400

    Add files via upload

---
+++ prog3-master/lesson 2/script.js
@@ -0,0 +1,50 @@
+var obj = {
+    "first_name": "Vardan",
+    "last_name": "hovsepyan",
+    "age": 13,
+    "tumo_student": true
+}
+
+class tumo_student {
+    constructor() {
+        this.first_name = "vardan";
+        this.age = 13
+        this.tumo_student = true;
+    }
+}
+
+var ogj = {
+    //first_name, last_name ..
+    sayHello() {
+        console.log("Hello")
+    }
+}
+console.log(obj);
+console.log(obh.first_name);
+obj.sayHello();
+
+
+function main() {
+    for (var i = 0; i < 10; i++) {
+        if (i == 2) {
+            console.log("Two");
+        }
+        console.log("Hello");
+        console.log(i);
+    }
+}
+main();
+for (var i in grassArr) {
+    grassArr[i].move();
+    grassArr[i].mul();
+    grassArr[i].die();
+}
+for (var i in grassArr) {
+    if (grassArr[i].x == x && grassArr[i].y == y) {
+        grassArr.splice(i, 1);
+        break;
+    }
+}
+
+
+
